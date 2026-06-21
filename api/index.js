657497d24673aed0d9659b4e3e76d18861c7fd1d
k-tanuj <tanujkumawat3008@gmail.com>
2026-06-21 14:33:49 +0530

    Fix Vercel 504 Timeout by exporting Web HTTP methods

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import server from '../dist/server/server.js';
 
-export default async function (request, context) {
+async function handler(request, context) {
   // Pass the request to the fetch handler exported by TanStack Start
   return server.default ? server.default.fetch(request, process.env, context) : server.fetch(request, process.env, context);
 }
+
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
+export const PATCH = handler;
+export const DELETE = handler;
+export const OPTIONS = handler;
+export const HEAD = handler;
