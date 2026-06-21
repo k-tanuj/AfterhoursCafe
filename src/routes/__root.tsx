03d6128c06ca2fdc840310a4d56d31521bf96493
k-tanuj <tanujkumawat3008@gmail.com>
2026-06-21 16:21:34 +0530

    Add afterhours logo as browser favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -93,6 +93,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { name: "twitter:site", content: "@afterhours" },
     ],
     links: [
+      { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
